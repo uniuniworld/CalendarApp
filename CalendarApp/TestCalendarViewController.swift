714c6ec0966aa42de999196d66e16f636a28cebd
kirifu_mini <uniuniunu2525@gmail.com>
2020-08-07 17:22:11 +0900

    no message

--- CalendarApp/TestCalendarViewController.swift
+++ CalendarApp/TestCalendarViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 import FSCalendar
 
 
-class TestCalendarViewController: UIViewController {
+class TestCalendarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FSCalendarDelegate, FSCalendarDataSource, UIGestureRecognizerDelegate {
+
+    
     
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var label: UILabel!
     
+    
+    @IBOutlet weak var animationSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        calendar.appearance.headerDateFormat = "MM月"
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        calendar.scrollDirection = .vertical
+        calendar.appearance.headerDateFormat = "MM月"
         
         // Do any additional setup after loading the view.
     }
@@ -31,12 +37,85 @@ class TestCalendarViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func week(_ sender: Any) {
-        calendar.scope = .week
+    
+    public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let tmpData = Calendar(identifier: .gregorian)
+        let year = tmpData.component(.year, from: date)
+        let month = tmpData.component(.month, from: date)
+        let day = tmpData.component(.day, from: date)
+        label.text = "\(year)/\(month)/\(day)"
     }
-    @IBAction func month(_ sender: Any) {
-        calendar.scope = .month
+    
+    @IBAction func selectToday(_ sender: Any) {
+        
+        self.calendar.select(Date())
+        label.text = "\(Date())"
+        
     }
+    
+    @IBAction func `switch`(_ sender: Any) {
+        if self.animationSwitch.isOn {
+            calendar.scrollDirection = .horizontal
+            print("on")
+        } else {
+            calendar.scrollDirection = .vertical
+            print("off")
+        }
+    }
+    
+    // MARK:- UITableViewDataSource
+    
+    /*
+     tableViewのセクション数
+     このメソッドを実装しない場合は、セクションは１つでテーブルを構成する
+     */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /*
+     必須 行数
+     tabaleViewの指定されたセクションの行数を返すようにDataSourceに指示する。
+     */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    // MARK:- UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.section == 0 {
+            
+        }
+    }
+    
+    
+    // MARK:- toggleClicked
+    @IBAction func toggleClicked(_ sender: Any) {
+        if self.calendar.scope == .month {
+            //self.calendar.setScope(.week, animated: self.animationSwitch.isOn)
+            self.calendar.scope = .week
+            print("week")
+        } else {
+            //self.calendar.setScope(.month, animated: self.animationSwitch.isOn)
+            self.calendar.scope = .month
+            print("month")
+        }
+    }
+    
+    
+
     
 }
 
@@ -55,14 +134,8 @@ class TestCalendarViewController: UIViewController {
 //
 //
 //}
+//
+//extension TestCalendarViewController: FSCalendarDelegate, FSCalendarDataSource {
+//
 
-extension TestCalendarViewController: FSCalendarDelegate, FSCalendarDataSource {
-    
-    public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let tmpData = Calendar(identifier: .gregorian)
-        let year = tmpData.component(.year, from: date)
-        let month = tmpData.component(.month, from: date)
-        let day = tmpData.component(.day, from: date)
-        print("\(year)/\(month)/\(day)")
-    }
-}
+
